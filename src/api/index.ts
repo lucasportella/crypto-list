acb80bbb38c692b7afdd625e4487f65229f3a92a
lucas <lucasportellaagu@gmail.com>
2024-03-17 09:45:08 -0300

    feat: api add integration with coingecko api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,16 @@
+export const BASE_URL = "https://api.coingecko.com/api/v3/"
+const API_KEY = import.meta.env.VITE_COINGECKO_API_KEY
+// need a server side like next js to not expose key
+
 const request = async (route: string, type: string) => {
   try {
     const response = await fetch(route, {
       method: type,
       headers: {
         'Content-Type': 'application/json',
-      }
+        "X-CoinAPI-Key": API_KEY
+      },
+
     });
     const data = await response.json();
     return data;
--- src/api/list.ts
+++ src/api/list.ts
@@ -1,8 +1,10 @@
-import { ICoin } from "../interfaces/apiTypes";
-import request from "./index";
-const BASE_URL = "https://api.coingecko.com/api/v3/"
+import { ICrypto } from "../interfaces/apiTypes";
+import request, { BASE_URL } from "./index";
+
+
+
 const requestCoinsList = () => {
-  const data: Promise<ICoin[]| unknown> = request(
+  const data: Promise<ICrypto[] | undefined> = request(
     `${BASE_URL}coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1`, "GET")
   return data
 }
