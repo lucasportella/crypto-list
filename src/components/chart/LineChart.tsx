1918709bae196a153e18ba247a1760b98289fd10
lucas <lucasportellaagu@gmail.com>
2024-03-19 20:26:40 -0300

    feat: add recharts library to display the 24h variation in price and volume in a chart and style it

---
+++ src/components/chart/LineChart.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { priceData } from "../../features/details/mock";
+import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, YAxis } from "recharts";
+import { formatDollarValue, formatHour } from "../../utils/formatString";
+import { CustomTooltipProps } from "../../interfaces/apiTypes";
+
+const CustomTooltip: React.FC<CustomTooltipProps> = ({ active, payload }) => {
+  if (active && payload && payload.length) {
+    const { time, price, volume } = payload[0].payload;
+    return (
+      <div className="tooltipStyles">
+        <p >{`Hour: ${time}`}</p>
+        <p>{`Price: $${formatDollarValue(price)}`}</p>
+        <p>{`Volume: $${volume}`}</p>
+      </div>
+    );
+  }
+  return null;
+};
+const Chart: React.FC = () => {
+  const data = priceData.prices.map((item, index) => (
+
+    {
+      time: formatHour(item[0]),
+      price: item[1],
+      market_caps: priceData.market_caps[index][1],
+      volume: formatDollarValue(priceData.total_volumes[index][1])
+    }
+  ))
+  const minYValue = Math.min(...data.map(entry => Math.min(entry.price)));
+  const maxYValue = Math.max(...data.map(entry => Math.max(entry.price)));
+  return (
+    <ResponsiveContainer width="100%" height={200}>
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      >
+        <YAxis dataKey="price" domain={[minYValue, maxYValue]} hide={true} />
+        <CartesianGrid strokeDasharray="1 2" />
+        <Tooltip content={<CustomTooltip />} />
+        <Line type="monotone" dataKey="price" stroke="#8884d8" dot={false} />
+        <Line type="monotone" dataKey="time" stroke="#82ca9d" dot={false} />
+        <Line type="monotone" dataKey="volume" stroke="#ffc658" dot={false} />
+      </LineChart>
+
+    </ResponsiveContainer>
+  );
+}
+export default Chart;
