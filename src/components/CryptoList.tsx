c040d22d5ddad4dd614075ae0b6f86aedc8362f9
Lucas Portella <lucasportellaagu@gmail.com>
2024-03-22 15:03:07 -0300

    feat: add reload button in homepage

--- src/components/CryptoList.tsx
+++ src/components/CryptoList.tsx
@@ -1,20 +1,17 @@
 import React from "react";
-import { selectCryptos } from "../features/cryptoList/cryptoListSlice";
+import { addCrypto, selectCryptos } from "../features/cryptoList/cryptoListSlice";
 import { useAppSelector } from "../app/hooks";
 import { useNavigate } from "react-router-dom";
 import { formatDollarValue } from "../utils/formatString";
 import { useAppDispatch } from "../app/hooks";
-import { addCryptoDetails } from "../features/cryptoDetails/CryptoSlice";
 import { ButtonsContainer } from "./nav/ButtonsContainer";
 const CryptoList: React.FC = () => {
   const cryptos = useAppSelector(selectCryptos)
   const dispatch = useAppDispatch()
-
-  const navigate = useNavigate()
-  const handleClick = (id: string) => {
-    navigate(`${id}`)
-    dispatch(addCryptoDetails(id))
+  const handleClick = () => {
+    dispatch(addCrypto({}))
   }
+  const navigate = useNavigate()
   const renderData = cryptos.map((item, index) => (
     <section key={item.id} className="flex border rounded-lg mb-4 w-full px-2 py-2 items-center gap-4 justify-between border-gray-600 lg:w-3/5 ">
       <div className="flex gap-2 items-center title">
@@ -37,7 +34,7 @@ const CryptoList: React.FC = () => {
       </div>
       <section className="flex items-center  gap-2">
 
-        <button className="home-button" onClick={() => { handleClick(item.id) }}>Details</button>
+        <button className="home-button" onClick={() => { navigate(`${item.id}`) }}>Details</button>
 
       </section>
     </section>
@@ -45,7 +42,7 @@ const CryptoList: React.FC = () => {
   return (
     <section className="border flex flex-col border-gray-600 px-2 items-center py-6 w-full self-center  rounded-md gap-2 overflow-auto h-4/5 lg:px-0  md:w-4/5">
       <div className="lg:w-3/5 w-full">
-        <ButtonsContainer text="refresh list" />
+        <ButtonsContainer text="refresh list" handleClick={handleClick} />
       </div>
       {renderData}
     </section>
