33269619761cf1ed09c795da97f62d82b286dfdb
lucas <lucasportellaagu@gmail.com>
2024-03-25 17:23:33 -0300

    test: homepage - enable cryptolist mock

--- src/pages/Home.spec.tsx
+++ src/pages/Home.spec.tsx
@@ -1,21 +1,17 @@
 import { vi, beforeEach, afterEach, describe, test, expect } from 'vitest'
-// import { mockedCryptoListResponse } from '../mocks/cryptoListResponse';
+import { mockedCryptoListResponse } from '../mocks/cryptoListResponse';
 import { renderWithProviders } from '../utils/test-utils/test-utils';
 import Home from './Home';
 import { screen } from '@testing-library/react';
 import { waitFor } from '@testing-library/react';
-
+import requestCryptoList from '../api/coingecko/cryptoList';
 describe(('Home page test'), () => {
   beforeEach(() => {
     vi.mock('react-router-dom', () => ({
       useNavigate: vi.fn()
     }))
-    // TODO: mock api
-    // vi.mock('../api/coingecko/cryptoList', () => ({
-    //   requestCryptoList: vi.fn().mockImplementation(async () => {
-    //     return mockedCryptoListResponse
-    //   }),
-    // }));
+    vi.mock('../api/coingecko/cryptoList')
+    vi.mocked(requestCryptoList).mockReturnValue(Promise.resolve(mockedCryptoListResponse))
   })
   afterEach(() => {
     vi.restoreAllMocks()
@@ -24,12 +20,13 @@ describe(('Home page test'), () => {
     renderWithProviders(<Home />)
 
   })
-  test('Renders async elements', async () => {
+  test('Renders async elements - coins and its prices', async () => {
     await waitFor(() => {
-      const { getAllByText } = renderWithProviders(<Home />)
-      expect(getAllByText(/bitcoin/i))
-      screen.debug(undefined, 99999999999)
-
+      const { getByText } = renderWithProviders(<Home />)
+      expect(getByText(/bitcoin/i))
+      expect(getByText(/65,767/))
+      expect(getByText(/ethereum/i))
+      expect(getByText(/3,514.61/))
     }, { timeout: 3000 });
   })
 })
