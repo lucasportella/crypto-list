f10c0ce71fc67ca92c0b74e4c8490d5564a91059
lucas <lucasportellaagu@gmail.com>
2024-03-25 21:39:48 -0300

    test: remove old import

--- src/pages/Home.spec.tsx
+++ src/pages/Home.spec.tsx
@@ -2,9 +2,9 @@ import { vi, beforeEach, afterEach, describe, test, expect } from 'vitest'
 import { mockedCryptoListResponse } from '../mocks/cryptoListResponse';
 import { renderWithProviders } from '../utils/test-utils/test-utils';
 import Home from './Home';
-import { screen } from '@testing-library/react';
 import { waitFor } from '@testing-library/react';
 import requestCryptoList from '../api/coingecko/cryptoList';
+
 describe(('Home page test'), () => {
   beforeEach(() => {
     vi.mock('react-router-dom', () => ({
@@ -13,13 +13,16 @@ describe(('Home page test'), () => {
     vi.mock('../api/coingecko/cryptoList')
     vi.mocked(requestCryptoList).mockReturnValue(Promise.resolve(mockedCryptoListResponse))
   })
+  
   afterEach(() => {
     vi.restoreAllMocks()
   })
+
   test('Renders Home page without crashing', () => {
     renderWithProviders(<Home />)
 
   })
+
   test('Renders async elements - coins and its prices', async () => {
     await waitFor(() => {
       const { getByText } = renderWithProviders(<Home />)
